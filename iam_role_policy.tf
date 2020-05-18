f86344cbd1c075d63fd5848be37b1191658982f6
Bhaskar855 <56507117+Bhaskar855@users.noreply.github.com>
2020-05-18 19:24:19 +0530

    Update iam_role_policy.tf

--- iam_role_policy.tf
+++ iam_role_policy.tf
@@ -1,4 +1,11 @@
-
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": "iam:CreateUser",
+    "Resource": "*"
+  }
+}
 # Role that pods can assume for access to elasticsearch and kibana
 resource "aws_iam_role" "elasticsearch_user" {
   name               = "module.user_label.id"
